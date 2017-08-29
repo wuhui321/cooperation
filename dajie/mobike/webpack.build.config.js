8cf38a281b19276b0bffd8d865824742e6bcabae
zhaoshiwei <zhaoshiwei@yidian-inc.com>
2017-08-29 11:25:11 +0800

    优化webpack结构

--- dajie/mobike/webpack.build.config.js
+++ dajie/mobike/webpack.build.config.js
@@ -3,9 +3,12 @@ const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const extractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-    entry: './src/scripts/main.js',
+    entry: {
+        main: path.resolve(__dirname, './src/scripts/main.js'),
+        video: path.resolve(__dirname, './src/scripts/video.js')
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -50,11 +53,17 @@ module.exports = {
         new htmlWebpackPlugin({
             template: './src/app.html',
             filename: 'app.html',
+            chunks: ['main'],
             title: '摩拜火星情报局，摩拜校招通关秘籍在这里！',
             shareTitle: '摩拜火星情报局，摩拜校招通关秘籍在这里！',
             shareDesc: '掌握摩拜校招全流程，了解摩拜的历程，畅谈摩拜的未来，飞速成长秘笈就在这里！',
             shareUrl: '',
             shareImg: 'https://ks3-cn-beijing.ksyun.com/static.toptest.yidianzixun.com/public/file/1503672810390/share.jpg'
+        }),
+        new htmlWebpackPlugin({
+            template: './src/video.html',
+            filename: 'video.html',
+            chunks: ['video']
         }),
         new extractTextPlugin("styles.css"),
         new webpack.ProvidePlugin({

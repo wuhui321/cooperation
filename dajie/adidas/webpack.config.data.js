88445262b760456fe04f9dee80f0a392d097e1e4
zhaoshiwei <zhaoshiwei@yidian-inc.com>
2017-09-13 21:43:28 +0800

    优化样式

--- dajie/adidas/webpack.config.data.js
+++ dajie/adidas/webpack.config.data.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: '打破界限',
-    shareTitle: '来阿迪，工作生活一起6，才是真正freestyle！',
-    shareDesc: '我潮流我自由我享受，我在阿迪等你一起freestyle！',
+    shareTitle: '阿迪达斯2018校园招聘',
+    shareDesc: '阿迪达斯2018校园招聘正式开启，精彩mv带你领略阿迪freestyle',
     shareUrl: '',
     shareImg: 'https://ks3-cn-beijing.ksyun.com/static.toptest.yidianzixun.com/public/file/1503672810390/share.jpg'
 };

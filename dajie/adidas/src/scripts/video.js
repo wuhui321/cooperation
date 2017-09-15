f030a1eed87e5f09da3f320955d9c45b38368614
zhaoshiwei <zhaoshiwei@yidian-inc.com>
2017-09-15 10:06:57 +0800

    更新内容，优化内存

--- dajie/adidas/src/scripts/video.js
+++ dajie/adidas/src/scripts/video.js
@@ -3,7 +3,7 @@ require("../css/animate.css");
 require("../less/adidasAni.less");
 require("../less/style.less");
 require("pixi.js");
-// require("./lib/sharewx.js");
+require("./lib/sharewx.js");
 var frame_start = [];
 var frame_loop = [
     require("../assets/images/content/frame/loop/p0.jpg"),
@@ -140,10 +140,10 @@ var webHandle = {
                 aniApp = self.aniApp;
 
 
-            frame_start.forEach(function(item) {
-                var tmp = PIXI.Texture.fromImage(item);
-                self.startTexture.push(tmp);
-            });
+            // frame_start.forEach(function(item) {
+            //     var tmp = PIXI.Texture.fromImage(item);
+            //     self.startTexture.push(tmp);
+            // });
             frame_loop.forEach(function(item) {
                 var tmp = PIXI.Texture.fromImage(item);
                 self.loopTexture.push(tmp);
@@ -154,10 +154,11 @@ var webHandle = {
             setInterval(function() {
                 if (self.flag && self.flag === 'start') {
                     if (self.imgCount < frame_start.length - 1) {
-                        self.aniSprite.texture = self.startTexture[self.imgCount];
+                        // self.aniSprite.texture = self.startTexture[self.imgCount];
+                        self.aniSprite.texture = PIXI.Texture.fromImage(frame_start[self.imgCount]);
                         console.log(self.imgCount);
                         if (self.imgCount === 35) {
-                            $("#adidasLogo").show();
+                            $("#adidasLogo, #pageText3").show();
                         }
                         self.imgCount++;
                     } else {
@@ -175,7 +176,7 @@ var webHandle = {
             }, 100);
             setTimeout(function() {
                 self.flag = "start";
-            }, 1000);
+            }, 300);
         },
         init: function() {
             var self = this,

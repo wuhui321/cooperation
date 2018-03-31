efa2ae26ba9b1e25325c9ace8b546fe0c89f3b15
zhaoshiwei <zhaoshiwei@yidian-inc.com>
2018-03-31 14:46:52 +0800

    update code

--- other/jiabao/webpack.config.data.js
+++ other/jiabao/webpack.config.data.js
@@ -2,6 +2,6 @@ module.exports = {
     title: '点击探索，给宝宝___的第一口',
     shareTitle: '点击探索，给宝宝___的第一口',
     shareDesc: '抢先感受嘉宝农场亲临视觉，参与探索开启红包大抽奖！',
-    shareUrl: 'http://m.letlike.com/case/2018/gerber/farmvr/testv1/app.html',
+    shareUrl: 'http://m.letlike.com/case/2018/gerber/farmvr/testv1/20180331/app.html',
     shareImg: 'https://ks3-cn-beijing.ksyun.com/static.toptest.yidianzixun.com/public/file/1522404328216/share.jpg'
 };
